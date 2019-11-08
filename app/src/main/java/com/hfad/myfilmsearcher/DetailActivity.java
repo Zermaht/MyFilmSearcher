cdfb910ac1c598c32c0c75987861d207676b6aa9
Zermaht <andruxa040@gmail.com>
2019-11-08 15:20:32 +0400

    doing second Activity

--- app/src/main/java/com/hfad/myfilmsearcher/DetailActivity.java
+++ app/src/main/java/com/hfad/myfilmsearcher/DetailActivity.java
@@ -2,13 +2,19 @@ package com.hfad.myfilmsearcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
+    private ImageView photo = findViewById(R.id.)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+
     }
 }
--- app/src/main/java/com/hfad/myfilmsearcher/MainActivity.java
+++ app/src/main/java/com/hfad/myfilmsearcher/MainActivity.java
@@ -2,6 +2,7 @@ package com.hfad.myfilmsearcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -16,18 +17,40 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onClickShoushenk(View view) {
-         TextView shoushenk = findViewById(R.id.textView_shoushenk);
-         shoushenk.setTextColor(getResources().getColor(R.color.colorAccent));
-    }
+    public void onClickButton(View view) {
+        int id = view.getId();
+        TextView mTextView = null;
+        int mImage = 0;
+        String mDescription = null;
 
-    public void onClickGreenMile(View view) {
-        TextView greenMile = findViewById(R.id.textView_zelenaia_milia);
-        greenMile.setTextColor(getResources().getColor(R.color.colorAccent));
-    }
+        switch (id) {
+            case R.id.button_shoushenk:
+                mTextView = findViewById(R.id.textView_shoushenk);
+                mImage = R.drawable.pobeg_iz_shoushenka;
+                mDescription = getResources().getString(R.string.shoushenk);
+                break;
+            case R.id.button_forest_gamp:
+                mTextView = findViewById(R.id.textView_forest_gump);
+                mImage = R.drawable.forest_gamp;
+                mDescription = getResources().getString(R.string.forest_gamp);
+                break;
+            case R.id.button_zelenaia_milia:
+                mTextView = findViewById(R.id.textView_zelenaia_milia);
+                mImage = R.drawable.zelenaia_milia;
+                mDescription = getResources().getString(R.string.zelenaia_milia);
+                break;
+        }
 
-    public void onClickGamp(View view) {
-        TextView mGamp = findViewById(R.id.textView_forest_gump);
-        mGamp.setTextColor(getResources().getColor(R.color.colorAccent));
+        if (mTextView != null && mImage != 0 && mDescription != null) {
+            mTextView.setTextColor(getResources().getColor(R.color.colorAccent));
+
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra("img", mImage);
+            intent.putExtra("imgDescription", mDescription);
+
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        }
     }
 }
