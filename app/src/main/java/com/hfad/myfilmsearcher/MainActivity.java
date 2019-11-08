3215c8f04988bd9f158712291a29b1aab67c88ad
Zermaht <andruxa040@gmail.com>
2019-11-08 14:40:32 +0400

    Seted methods for buttons to change color of the title

--- app/src/main/java/com/hfad/myfilmsearcher/MainActivity.java
+++ app/src/main/java/com/hfad/myfilmsearcher/MainActivity.java
@@ -3,12 +3,31 @@ package com.hfad.myfilmsearcher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClickShoushenk(View view) {
+         TextView shoushenk = findViewById(R.id.textView_shoushenk);
+         shoushenk.setTextColor(getResources().getColor(R.color.colorAccent));
+    }
+
+    public void onClickGreenMile(View view) {
+        TextView greenMile = findViewById(R.id.textView_zelenaia_milia);
+        greenMile.setTextColor(getResources().getColor(R.color.colorAccent));
+    }
+
+    public void onClickGamp(View view) {
+        TextView mGamp = findViewById(R.id.textView_forest_gump);
+        mGamp.setTextColor(getResources().getColor(R.color.colorAccent));
     }
 }
