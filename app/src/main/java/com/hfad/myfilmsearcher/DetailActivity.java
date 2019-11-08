5658ee77799c8009b7113dd658c07fba6507a0dc
Zermaht <andruxa040@gmail.com>
2019-11-08 16:20:38 +0400

    сохранение цвета при перевороте

--- app/src/main/java/com/hfad/myfilmsearcher/DetailActivity.java
+++ app/src/main/java/com/hfad/myfilmsearcher/DetailActivity.java
@@ -5,16 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
-    private ImageView photo = findViewById(R.id.)
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        Intent intent = getIntent();
+        ImageView dPhoto = findViewById(R.id.img_detail_photo);
+        TextView dTextView = findViewById(R.id.textView2);
 
+        Intent intent = getIntent();
+        int dDescriptionID = intent.getIntExtra("imgDescription", 0);
+
+        dPhoto.setImageResource(intent.getIntExtra("img", 0));
+        dPhoto.setContentDescription(getResources().getString(dDescriptionID));
+
+        if (dDescriptionID != 0) {
+            switch (dDescriptionID) {
+                case R.string.shoushenk:
+                    dTextView.setText(getResources().getString(R.string.shoushenk_opisanie));
+                    break;
+                case R.string.forest_gamp:
+                    dTextView.setText(getResources().getString(R.string.forest_gamp_opisanie));
+                    break;
+                case R.string.zelenaia_milia:
+                    dTextView.setText(getResources().getString(R.string.zelenaia_milia_opisanie));
+                    break;
+            }
+        }
     }
 }
--- app/src/main/java/com/hfad/myfilmsearcher/MainActivity.java
+++ app/src/main/java/com/hfad/myfilmsearcher/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hfad.myfilmsearcher;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,48 +10,68 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mShoushenkTextView;
+    private TextView mGreenMile;
+    private TextView mGamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+         mShoushenkTextView = findViewById(R.id.textView_shoushenk);
+         mGreenMile = findViewById(R.id.textView_zelenaia_milia);
+         mGamp = findViewById(R.id.textView_forest_gump);
+
+        if (savedInstanceState != null) {
+            mShoushenkTextView.setTextColor(savedInstanceState.getInt("Shoushenk_color"));
+            mGreenMile.setTextColor(savedInstanceState.getInt("Green_mile_color"));
+            mGamp.setTextColor(savedInstanceState.getInt("Gamp_color"));
+        }
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("Shoushenk_color", mShoushenkTextView.getCurrentTextColor());
+        outState.putInt("Green_mile_color", mGreenMile.getCurrentTextColor());
+        outState.putInt("Gamp_color", mGamp.getCurrentTextColor());
     }
 
     public void onClickButton(View view) {
         int id = view.getId();
         TextView mTextView = null;
         int mImage = 0;
-        String mDescription = null;
+        int mDescription = 0;
 
         switch (id) {
             case R.id.button_shoushenk:
-                mTextView = findViewById(R.id.textView_shoushenk);
+                mTextView = mShoushenkTextView;
                 mImage = R.drawable.pobeg_iz_shoushenka;
-                mDescription = getResources().getString(R.string.shoushenk);
+                mDescription = R.string.shoushenk;
                 break;
             case R.id.button_forest_gamp:
-                mTextView = findViewById(R.id.textView_forest_gump);
+                mTextView = mGamp;
                 mImage = R.drawable.forest_gamp;
-                mDescription = getResources().getString(R.string.forest_gamp);
+                mDescription = R.string.forest_gamp;
                 break;
             case R.id.button_zelenaia_milia:
-                mTextView = findViewById(R.id.textView_zelenaia_milia);
+                mTextView = mGreenMile;
                 mImage = R.drawable.zelenaia_milia;
-                mDescription = getResources().getString(R.string.zelenaia_milia);
+                mDescription = R.string.zelenaia_milia;
                 break;
         }
 
-        if (mTextView != null && mImage != 0 && mDescription != null) {
+        if (mTextView != null && mImage != 0 && mDescription != 0) {
             mTextView.setTextColor(getResources().getColor(R.color.colorAccent));
 
             Intent intent = new Intent(this, DetailActivity.class);
             intent.putExtra("img", mImage);
             intent.putExtra("imgDescription", mDescription);
 
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                startActivity(intent);
-            }
+            startActivity(intent);
+
         }
     }
 }
