1a8a112bb7f5d720aefc8ccaa94f5359e1dfb135
Zermaht <andruxa040@gmail.com>
2019-11-17 17:53:48 +0400

    Добавляем NavigetionDrawer

---
+++ app/src/main/java/com/hfad/myfilmsearcher/CaptionedImageAdapter.java
@@ -0,0 +1,76 @@
+package com.hfad.myfilmsearcher;
+
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CaptionedImageAdapter extends RecyclerView.Adapter<CaptionedImageAdapter.ViewHolder> {
+
+    private String[] filmName;
+    private int[] imageId;
+    private Listener listener;
+
+    interface Listener {
+        void onClick(int position);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view, parent, false);
+
+        return new ViewHolder(cv);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        CardView cardView = holder.cardView;
+        ImageView imageView = cardView.findViewById(R.id.info_image);
+        Drawable drawable = ContextCompat.getDrawable(cardView.getContext(), imageId[position]);
+        imageView.setImageDrawable(drawable);
+        imageView.setContentDescription(filmName[position]);
+        TextView textView = cardView.findViewById(R.id.info_film);
+        textView.setText(filmName[position]);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null){
+                    listener.onClick(position);
+                }
+            }
+        });
+    }
+
+    public CaptionedImageAdapter (String[] filmName, int[] imageId) {
+        this.filmName = filmName;
+        this.imageId = imageId;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+
+    public void setListener(Listener listener){
+        this.listener = listener;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        private CardView cardView;
+
+        public ViewHolder(CardView v) {
+            super(v);
+            cardView = v;
+        }
+    }
+}
